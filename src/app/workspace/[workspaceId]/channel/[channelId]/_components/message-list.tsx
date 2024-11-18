7e9a298828c873efcd3a31e51a129539e6747f99
Pedro Henrique <pedrohalmeidamendonca@gmail.com>
2024-11-18 16:27:47 -0300

    Implementing infinite message load effect

--- src/app/workspace/[workspaceId]/channel/[channelId]/_components/message-list.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/_components/message-list.tsx
@@ -1,4 +1,5 @@
 import { differenceInMinutes, format } from "date-fns";
+import { Loader2 } from "lucide-react";
 import { useParams } from "next/navigation";
 import { useState } from "react";
 
@@ -103,6 +104,32 @@ export const MessageList = ({
           })}
         </div>
       ))}
+      <div
+        className="h-1"
+        ref={(element) => {
+          if (element) {
+            const observer = new IntersectionObserver(
+              ([entry]) => {
+                if (entry.isIntersecting && canLoadMore) {
+                  loadMore();
+                }
+              },
+              { threshold: 1.0 }
+            );
+            observer.observe(element);
+
+            return () => observer.disconnect();
+          }
+        }}
+      />
+      {isLoadingMore && (
+        <div className="text-center my-2 relative">
+          <hr className="absolute top-1/2 left-0 right-0 border-t border-gray-300" />
+          <span className="relative inline-block bg-white px-4 py-1 rounded-full text-xs border border-gray-300 shadow-sm">
+            <Loader2 className="size-4 animate-spin" />
+          </span>
+        </div>
+      )}
       {variant === "channel" && channelName && channelCreationTime && (
         <ChannelHero name={channelName} creationTime={channelCreationTime} />
       )}
